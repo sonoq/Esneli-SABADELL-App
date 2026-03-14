04d8a97a1d0d81b82ddd029d4c4248016fec37e3
oscar <oscarperibanyez@gmail.com>
2026-03-14 21:18:54 +0100

    Initial commit with PWA support

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'turnos-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    self.clients.claim();
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
